63cefe222951a0545c6e536dec4a6d49e6095bf7
Hildorien <nachofernandez.1995@hotmail.com>
2017-05-17 14:41:05 -0300

    Anda script para el ej3

--- Scripts/Ej3/correrExp_Ej3.sh
+++ Scripts/Ej3/correrExp_Ej3.sh
@@ -2,11 +2,11 @@
 mkdir -p ../../Outputs/Ej3/outsTiempo
 mkdir -p ../../Inputs/Ej3/insTiempo
 
-tams=(5 10 20 50 75 100)
-for((i = 0 ; i < 6; i+=1));do
+tams=(5 10 20 50 75 100 200)
+for((i = 0 ; i < 7; i+=1));do
 	# echo ${i}
 	printf '%s\n' "${tams[${i}]} "
-	python randomKn.py ${tams[i]} 2 > ../../Inputs/Ej3/insTiempo/ej3_${tams[i]}
-	#../../tiempo 3 < ../../Inputs/Ej3/insTiempo/ej3_${tams[i]} >  ../../Outputs/Ej3/outsTiempo/prueba_${tams[i]}.csv
+	python randomKn.py ${tams[i]} 50 > ../../Inputs/Ej3/insTiempo/ej3_${tams[i]}
+	../../tiempo 3 < ../../Inputs/Ej3/insTiempo/ej3_${tams[i]} >  ../../Outputs/Ej3/outsTiempo/prueba_${tams[i]}.csv
 done
 printf '%s\n'
