0ffc753c92ea0a62e335b64f0da4d34baf940746
Hildorien <nachofernandez.1995@hotmail.com>
2017-05-19 14:07:48 -0300

    Cambie los nombres de los generadores de exper. + agregue script para exp 1.2

--- Scripts/Ej3/correrExp1_Ej3.sh
+++ Scripts/Ej3/correrExp1_Ej3.sh
@@ -10,7 +10,7 @@ tams=(5 10 20 50 75 100 200)
 for((i = 0 ; i < ${#tams[@]}; i+=1));do
 	# echo ${i}
 	printf '%s\n' "${tams[${i}]} "
-	python randomKn.py ${tams[i]} 50 > ../../Inputs/Ej3/insTiempo/exp1/ej3_exp1_${tams[i]}
+	python random_ejer3_exp1.py ${tams[i]} 50 > ../../Inputs/Ej3/insTiempo/exp1/ej3_exp1_${tams[i]}
 	../../tiempo 3 < ../../Inputs/Ej3/insTiempo/exp1/ej3_exp1_${tams[i]} >  ../../Outputs/Ej3/outsTiempo/exp1/prueba_exp1_${tams[i]}.csv
 done
 printf '%s\n'
--- Scripts/Ej3/correrExp2_Ej3.sh
+++ Scripts/Ej3/correrExp2_Ej3.sh
@@ -10,7 +10,7 @@ perct=(0 5 10 25 50 75 100 )
 for((i = 0 ; i < ${#perct[@]}; i+=1));do
 	# echo ${i}
 	printf '%s\n' "${perct[${i}]} "
-	python generadorPerctConstruidas.py ${perct[i]} 50 > ../../Inputs/Ej3/insTiempo/exp2/ej3_exp2_${perct[i]}
+	python random_ejer3_exp2.py ${perct[i]} 50 > ../../Inputs/Ej3/insTiempo/exp2/ej3_exp2_${perct[i]}
 	../../tiempo 3 < ../../Inputs/Ej3/insTiempo/exp2/ej3_exp2_${perct[i]} >  ../../Outputs/Ej3/outsTiempo/exp2/prueba_exp2_${perct[i]}.csv
 done
 printf '%s\n'
