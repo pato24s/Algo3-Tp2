1117ef8d84e38a6b3d5ae0790ea646768517ca54
Fernando Balboa <fbalboa95@gmail.com>
2017-05-24 12:31:27 -0300

    Graficando ej1_exp2

--- Scripts/Ej1/correrEj1_exp2.sh
+++ Scripts/Ej1/correrEj1_exp2.sh
@@ -7,14 +7,14 @@ cd $current
 mkdir -p ../../Inputs/Ej1/insTiempo/exp2
 mkdir -p ../../Outputs/Ej1/outsTiempo/exp2
 
-pct_premiums=(100 100 100 50 50 50 0 5 10 15 20)
-pct_k=(100 50 0 100 50 0 0 100 10 5 2)
+cant_caminos=(5 5 5 5 5 20 20 20 20 20)
+k=(1 2 3 4 5 1 5 10 15 20)
 cant_instancias=50
 
-for((i = 0 ; i < ${#pct_premiums[@]}; i+=1));do
+for((i = 0 ; i < ${#cant_caminos[@]}; i+=1));do
 	# echo ${i}
 	
-	printf '%s\n' "${pct_premiums[${i}]}, ${pct_k[${i}]} "
-	python ej1_exp2.py ${pct_premiums[i]} ${pct_k[i]} $cant_instancias > ../../Inputs/Ej1/insTiempo/exp2/ej1_exp2_${pct_premiums[i]}_${pct_k[i]}
-	../../tiempo 1 < ../../Inputs/Ej1/insTiempo/exp2/ej1_exp2_${pct_premiums[i]}_${pct_k[i]} >  ../../Outputs/Ej1/outsTiempo/exp2/prueba_exp2_${pct_premiums[i]}_${pct_k[i]}.csv
+	printf '%s\n' "${cant_caminos[${i}]}, ${k[${i}]} "
+	python ej1_exp2.py ${cant_caminos[i]} ${k[i]} $cant_instancias > ../../Inputs/Ej1/insTiempo/exp2/ej1_exp2_${cant_caminos[i]}_${k[i]}
+	../../tiempo 1 < ../../Inputs/Ej1/insTiempo/exp2/ej1_exp2_${cant_caminos[i]}_${k[i]} >  ../../Outputs/Ej1/outsTiempo/exp2/ej1_exp2_${cant_caminos[i]}_${k[i]}.csv
 done
